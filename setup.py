877531316b6a5aa216fb667beceddea59b908007
Christer <christer.gronblad@gmail.com>
2024-08-07 19:50:28 +0200

    add missing fields

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name="mkdocs-pipeline-visualizer",
     version="0.1.0",
     description="generate MD file from tekton pipelines and tasks, intended to be used together with mkdocs-techdocs-core",
+    long_description="# not-yet",
+    long_description_content_type="text/markdown",
     url="https://github.com/obegron/mkdocs-pipeline-visualizer",
     packages=find_packages(),
     author='Christer Grönblad',
